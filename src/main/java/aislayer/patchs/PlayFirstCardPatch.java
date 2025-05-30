2f33c62366608f796dd0d28f9f2735068d3b8b91
河童 <1472026755@qq.com>
2025-05-30 17:47:04 +0800

    添加思考状态的随机对话文本，支持多语言本地化

--- src/main/java/aislayer/patchs/PlayFirstCardPatch.java
+++ src/main/java/aislayer/patchs/PlayFirstCardPatch.java
@@ -6,8 +6,13 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.animations.TalkAction;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
 
 import static aislayer.AISlayer.getInfo;
 import static aislayer.AISlayer.isAIStart;
@@ -29,7 +34,11 @@ public class PlayFirstCardPatch {
         ) {
             AISlayer.intentUpdated = true;
 
-            AbstractDungeon.actionManager.addToBottom(new TalkAction(true, "我先琢磨琢磨...", 4.0F, 4.0F));
+            String langPackDir = "aislayerResources" + File.separator + "localization" + File.separator + Settings.language.toString().toLowerCase();
+            String textPath = langPackDir + File.separator + "text.json";
+            JSONArray text = (new JSONObject(AISlayer.loadJson(textPath))).getJSONArray("thinking");
+            String thinking = text.getString((int) (Math.random() * text.length()));
+            AbstractDungeon.actionManager.addToBottom(new TalkAction(true, thinking, 4.0F, 4.0F));
 
             String todo = "现在你可以选择使用药水、打出手牌、结束回合";
             AIUtils.action(getInfo(todo));
